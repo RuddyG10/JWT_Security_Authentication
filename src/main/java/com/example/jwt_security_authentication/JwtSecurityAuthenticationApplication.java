02f1cb7421b658340a2a458670eb5d58c42ead2e
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2024-03-01 10:38:51 -0400

    jwt and jakarta dependencies added

---
+++ src/main/java/com/example/jwt_security_authentication/JwtSecurityAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_security_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSecurityAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jwt_security_authentication/JwtSecurityAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_security_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

27156ff3528c3a8d0f67068975bc0de262a19855
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2024-03-01 13:22:06 -0400

    Added a commandLineRunner to create the roles and save them into the database automatically

--- src/main/java/com/example/jwt_security_authentication/JwtSecurityAuthenticationApplication.java
+++ src/main/java/com/example/jwt_security_authentication/JwtSecurityAuthenticationApplication.java
@@ -1,7 +1,15 @@
 package com.example.jwt_security_authentication;
 
+import com.example.jwt_security_authentication.Service.IUserService;
+import com.example.jwt_security_authentication.models.Role;
+import com.example.jwt_security_authentication.models.RoleName;
+import com.example.jwt_security_authentication.reposiroties.RoleRepository;
+import com.example.jwt_security_authentication.reposiroties.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class JwtSecurityAuthenticationApplication {
@@ -9,5 +17,10 @@ public class JwtSecurityAuthenticationApplication {
     public static void main(String[] args) {
         SpringApplication.run(JwtSecurityAuthenticationApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner run (IUserService iUserService , RoleRepository roleRepository , UserRepository userRepository , PasswordEncoder passwordEncoder) {
+        return  args -> {   iUserService.saveRole(new Role(RoleName.USER));
+        iUserService.saveRole(new Role(RoleName.ADMIN));
+    };
+    }
 }
